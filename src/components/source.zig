33b4864700219d1695577df9c419992bf28e1dfa
efoppiano <efoppiano@fi.uba.ar>
2024-03-26 22:24:15 -0300

    Refactor source.zig

--- src/components/source.zig
+++ src/components/source.zig
@@ -4,33 +4,78 @@ const Queue = @import("queue.zig").Queue;
 const ChunkReader = @import("../chunk_reader.zig").ChunkReader;
 const Message = @import("message.zig").Message;
 
-const BUFFER_SIZE = 1000000;
+pub fn Source(comptime T: type) type {
+    return struct {
+        queue: *Queue(T),
+        buffer_size: usize,
+        allocator: std.mem.Allocator,
 
-pub fn initiate_source(input_queue: *Queue(Message)) !void {
-    const input_filename = "data/input.txt";
-    const input_file = try std.fs.cwd().openFile(input_filename, .{});
-    defer input_file.close();
+        const Self = @This();
 
-    var buffered_reader = std.io.bufferedReader(input_file.reader());
-    var br = buffered_reader.reader();
-    var chunk_reader = ChunkReader.init(true);
-
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    var arena_allocator = arena.allocator();
-    const buffer = arena_allocator.alloc([4 * c.N_B]u8, BUFFER_SIZE) catch return error.OutOfMemory;
-
-    var next_pos: u64 = 0;
-
-    while (true) {
-        const chunks_filled = try chunk_reader.read_chunks(br, BUFFER_SIZE, buffer);
-        if (chunks_filled == 0) {
-            break;
+        pub fn init(queue: *Queue(T), buffer_size: usize, allocator: std.mem.Allocator) Self {
+            return Self{
+                .queue = queue,
+                .buffer_size = buffer_size,
+                .allocator = allocator,
+            };
         }
-        for (buffer[0..chunks_filled]) |block| {
-            const m = Message.init(block, next_pos);
-            //std.debug.print("{s}", .{block});
-            try input_queue.push(m);
-            next_pos += 1;
+
+        pub fn run_from_reader(self: *Self, with_padding: bool, input: anytype) !void {
+            var buffered_reader = std.io.bufferedReader(input);
+            var br = buffered_reader.reader();
+            var chunk_reader = ChunkReader.init(with_padding);
+
+            const buffer = self.allocator.alloc([4 * c.N_B]u8, self.buffer_size) catch return error.OutOfMemory;
+            defer self.allocator.free(buffer);
+
+            var next_pos: u64 = 0;
+
+            while (true) {
+                const chunks_filled = try chunk_reader.read_chunks(br, self.buffer_size, buffer);
+                if (chunks_filled == 0) {
+                    break;
+                }
+                for (buffer[0..chunks_filled]) |block| {
+                    const m = Message.init(block, next_pos);
+                    try self.queue.push(m);
+                    next_pos += 1;
+                }
+            }
         }
-    }
+
+        pub fn run_from_file(self: *Self, with_padding: bool, input_file_path: []const u8) !void {
+            const input_file = try std.fs.cwd().openFile(input_file_path, .{});
+            defer input_file.close();
+            try self.run_from_reader(with_padding, input_file.reader());
+        }
+    };
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
